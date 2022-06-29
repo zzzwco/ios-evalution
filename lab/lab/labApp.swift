e7c98555df39bf86c8596f42f61b15340851e1af
zzzwco <zzzwco@outlook.com>
2022-06-29 09:23:39 +0800

    Add project Lab

---
+++ lab/lab/labApp.swift
@@ -0,0 +1,18 @@
+//
+//  LabApp.swift
+//  Lab
+//
+//  Created by Bruce on 2022/6/29.
+//
+
+import SwiftUI
+
+@main
+struct LabApp: App {
+  
+  var body: some Scene {
+    WindowGroup {
+      HomeView()
+    }
+  }
+}
